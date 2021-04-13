da287fbd9a68b0fbd6959462d777841418ba941b
shamolmojumder <shamolmojumder14@gmail.com>
2021-04-13 14:59:57 +0600

    component done

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,13 +1,55 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
 
 const Post = (props) => {
     console.log(props);
-    const {title}=props.post
-    return (
-        <div>
+    const {title,id,body}=props.post
+    const classes = useStyles();
 
-            <h1>Post tilte: {title} </h1>            
-        </div>
+    return (
+        <Card className={classes.root}>
+    <CardActionArea>
+      
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+               {
+                   title
+               }
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {
+                  id
+              }
+            </Typography>
+
+            <Typography>
+                Discription:{
+                    body
+                }
+            </Typography>
+        </CardContent>
+    </CardActionArea>
+    <CardActions>
+        <Button size="small" color="primary">
+        <a href>Learn More</a>
+        </Button>
+    </CardActions>
+</Card>
     );
 };
 
